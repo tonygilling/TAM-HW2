796bb8c166077c14ce7d14bc4ff4e70b61263425
libra guest <tonygilling@hotmail.com>
2012-08-01 07:34:59 -0400

    hw2 done

--- tests/1_equality.js
+++ tests/1_equality.js
@@ -7,21 +7,21 @@
     describe("About Equality", function () {
         describe("double equals", function () {
             it("should auto type cast an integer and a string to be true", function () {
-                expect(5 == "5").to.be(FILL_ME_IN);
+                expect(5 == "5").to.be(true);
             });
     
             it("should auto type cast a float and a string to be true", function () {
-                expect(5.0 == "5.0").to.be(FILL_ME_IN);
+                expect(5.0 == "5.0").to.be(true);
             });
         });
     
         describe("triple equals", function () {
             it("should auto type cast an integer and a string to be true", function () {
-                expect(5 === "5").to.be(FILL_ME_IN);
+                expect(5 === "5").to.be(false);
             });
     
             it("should auto type cast a float and a string to be true", function () {
-                expect(5.0 === "5.0").to.be(FILL_ME_IN);
+                expect(5.0 === "5.0").to.be(false);
             });
         });
     });
